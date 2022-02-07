a7b402011f8656b97b3d80503d965107051a5cab
kuzin455 <kuzin455@gmail.com>
2022-02-07 14:56:36 +0300

    добавил key

--- src/Components/Table/index.js
+++ src/Components/Table/index.js
@@ -1,35 +1,32 @@
 import React from "react";
 import styles from "./index.module.css";
-import {CASE_DECLENSION_MAP} from './const'
+import { CASE_DECLENSION_MAP } from "./const";
 
+const Table = ({ dataBody, dataHeader }) => {
+  return (
+    <div className={styles.table}>
+      <div className={styles.headerRow}>
+        {dataHeader.map((item) => (
+          <div key={item} className={styles.cell}>{item}</div>
+        ))}
+      </div>
 
-const Table = ({dataBody, dataHeader}) => {
-     return (
-        <div className={styles.table}>
-            <div className={styles.headerRow}>
-                {dataHeader.map((item) => (<div className={styles.cell}>{item}</div>))}
+      <div className={styles.body}>
+        {dataBody.map(({ name, item }) => {
+          return (
+            <div className={styles.bodyRow}>
+              <div  className={styles.cell}>
+                {CASE_DECLENSION_MAP[name]}
+              </div>
+              <div  className={styles.cell}>
+                {item}
+              </div>
             </div>
-
-            <div className={styles.body}>
-                {dataBody.map(({name, item}) => {
-                    return (
-                        <div className={styles.bodyRow}>
-                            <div className={styles.cell}>{CASE_DECLENSION_MAP[name]}</div>
-                            <div className={styles.cell}>{item}</div>
-                        </div>)
-                })}
-            </div>
-        </div>
-    )
-
-
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Table;
-
-
-
-
-
-
-
